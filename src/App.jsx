3f4c906136c7c22ad7a57a26e882cb80a2a1ea64
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-09-26 15:48:09 +0200

    adds core exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
 
-import './styles/App.css'
+import "./styles/App.css";
+
+function RenderEmails({ emails }) {
+  return emails.map((email) => {
+    return (
+      <li className="email" key={email.id}>
+        <div className="select">
+          <input className="select-checkbox" type="checkbox" />
+        </div>
+        <div className="star">
+          <input className="star-checkbox" type="checkbox" />
+        </div>
+        <div className="sender">{email.sender}</div>
+        <div className="title">{email.title}</div>
+      </li>
+    );
+  });
+}
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
+  const [emails, setEmails] = useState(initialEmails);
 
   return (
     <div className="app">
@@ -17,7 +36,7 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{initialEmails.length}</span>
           </li>
           <li
             className="item"
@@ -28,7 +47,7 @@ function App() {
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
@@ -38,9 +57,13 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul>
+          <RenderEmails emails={emails}/>
+        </ul>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
